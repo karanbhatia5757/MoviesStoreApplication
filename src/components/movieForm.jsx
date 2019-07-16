e2b0f527c686861db83f13f3de691ee90a959c73
Karan Bhatia <karanbhatia@Karans-MBP.home>
2019-07-16 18:31:19 -0400

    Connected Movie Form with Backend and deleted fake services.

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getMovie, saveMovie } from "../services/movieService";
+import { getGenres } from "../services/genreService";
 
 class MovieForm extends Form {
   state = {
@@ -36,19 +36,26 @@ class MovieForm extends Form {
       .label("Daily Rental Rate")
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async populateGenres() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
+  async populateMovie() {
+    try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.maptoViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
+  }
 
-    if (movieId === "new") return;
-
-    const movie = getMovie(movieId);
-
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.maptoViewModel(movie) });
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   maptoViewModel(movie) {
@@ -60,6 +67,11 @@ class MovieForm extends Form {
       dailyRentalRate: movie.dailyRentalRate
     };
   }
+
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
+    this.props.history.push("/movies");
+  };
 
   render() {
     return (
