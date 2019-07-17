abd88dfb1b8cb7dd37dc100c7b1a1fc2cd369348
Karan Bhatia <karanbhatia@Karans-MBP.home>
2019-07-17 11:39:17 -0400

    Removed console log.

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -27,12 +27,9 @@ class RegisterForm extends Form {
       await userService.register(this.state.data);
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
-        console.log("Hello ji");
         const errors = { ...this.state.errors };
         errors.username = ex.response.data;
         this.setState({ errors });
-        console.log(this.state.errors);
-        console.log("Hello jii");
       }
     }
   };
